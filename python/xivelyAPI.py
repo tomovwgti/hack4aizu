e344395ee35d5dd1c4b171eb20830b1250bbee43
tomo watanabe <t.watanabe@freebit.net>
2014-02-03 18:04:56 +0900

    rename xively_test.py

--- python/xivelyAPI.py
+++ python/xivelyAPI.py
@@ -4,7 +4,7 @@ __author__ = 'tomo'
 import xively
 import datetime
 
-api = xively.XivelyAPIClient("QpKpTZHevDyfyADEu7bwECsJCiSyB4x57M8C9UFawDODZdEc")
+api = xively.XivelyAPIClient("API-Key")
 feed = api.feeds.get(1495248008)
 list(feed.datastreams)
 
